84869aed6bd305b02213ffebf407b16937308eeb
Michael Goldman <michael.s.goldman@vanderbilt.edu>
2014-05-15 13:57:10 -0400

    Added FAQ page

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   get "about" => "pages#about"
 
+  get "faq" => "pages#faq"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
